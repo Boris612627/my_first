fdaab245dfd7da3128249dc316ce9b5220b324a1
Boris612627 <br-ek-sv@mail.ru>
2026-02-24 13:27:27 +0300

    j

--- 4/4-3.py
+++ 4/4-3.py
@@ -1,17 +1,26 @@
 import RPi.GPIO as gpio
 gpio.setmode(gpio.BCM)
 class PWM_DAC:
-    def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose = False):
+    def __inint__(self, gpio_pin, pwm_frequency, dynamic_range, verbose=False):
         self.gpio_pin = gpio_pin
         self.pwm_frequency = pwm_frequency
         self.dynamic_range = dynamic_range
-    
+        self.verbose = verbose
+
+        gpio.setup(self.gpio_pin, gpio.OUT, initial=0)
+
+
     def deinit(self):
         gpio.output(self.gpio_pin, 0)
         gpio.cleanup()
 
     def set_voltage(self, voltage):
-        gpio.output(self.gpio_pin, voltage)
+        if not (0.0 <= voltage <= self.dynamic_range):
+            print(f"Напряжение выходит за динамический диапозон ЦАП (0.00 - {self.dynamic_range:.2f} B)")
+            print("Устонавливаем 0.0 B")
+            return 0
+        pwm = gpio.PWM(self.gpio_pin, self.pwm_frequency)
+        pwm.start(voltage)
 
 
 if __name__ == "__main__":
@@ -25,6 +34,6 @@ if __name__ == "__main__":
 
             except ValueError:
                 print("Вы ввели не число. Попробуйте еще раз\n")
-    
+
     finally:
         dac.deinit()
