66f56b774eed3eccb8b54ff40e2ef8611280b4ec
Boris612627 <br-ek-sv@mail.ru>
2026-03-16 19:28:15 +0300

    hh

--- 5/r2r-sc.py
+++ 5/r2r-sc.py
@@ -1,13 +1,37 @@
-import r2r_adc as r2r
 import time
-import matplotlib.pyplot as plt
+from r2r_adc import R2R_ADC
+from adc_plot import plot_voltage_vs_time
 
-adc = r2r.R2R_ADC(3.1, 0.0001)
-voltage_values = []
-time_values = []
-duration = 3.0
-
-try:
-    st = time.time()
-    while (time.time() - st) < duration:
+def main():
+    DYNAMIC_RANGE = 3.3      
+    COMPARE_TIME = 0.0001   
+    DURATION = 3.0
+    voltage_values = []
+    time_values = []
+    
+    adc = None
+    
+    try:
+        adc = R2R_ADC(DYNAMIC_RANGE, compare_time=COMPARE_TIME, verbose=False)
         
+        start_time = time.time()
+        last_print = start_time
+        
+        while time.time() - start_time < DURATION:
+            current_time = time.time() - start_time
+            
+            voltage = adc.get_sc_voltage()
+            
+            time_values.append(current_time)
+            voltage_values.append(voltage)
+            if current_time - last_print >= 0.5:
+                print(f"Время: {current_time:.2f} с, U = {voltage:.3f} В")
+                last_print = current_time
+        
+        plot_voltage_vs_time(time_values, voltage_values, DYNAMIC_RANGE)
+    finally:
+        if adc:
+            adc.deinit()
+
+if __name__ == "__main__":
+    main()
